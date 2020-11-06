4d280b903059ad72c7205a88a4b6fcd93c7d8114
kkawahar <xi.reich8@gmail.com>
2020-11-06 10:51:44 +0900

    Update sd-local.rb

--- sd-local.rb
+++ sd-local.rb
@@ -3,9 +3,9 @@ require "formula"
 class SdLocal < Formula
   desc "sd-local"
   homepage "https://screwdriver.cd/"
-  version "1.0.11"
+  version "1.0.12"
   url "https://github.com/screwdriver-cd/sd-local/releases/download/v#{version}/sd-local_darwin_amd64"
-  sha256 "5a97e57773bbf11d1500e5c1dc12fda88f338e86865494f86e892236986f605b"
+  sha256 "7f7fbd6564cc68c2f3d286f5293d922a48a8d9a7e80b685c8ae6855a629c80ec"
 
   def install
     bin.install "sd-local_darwin_amd64" => "sd-local"
