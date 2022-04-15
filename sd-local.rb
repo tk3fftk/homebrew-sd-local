e677cba3d63577aa193b6b8cdbb0b234e47ea02a
hitakats <hitakats@yahoo-corp.jp>
2022-04-15 17:26:10 +0900

    use universal binary

--- sd-local.rb
+++ sd-local.rb
@@ -1,13 +1,15 @@
-require "formula"
+# frozen_string_literal: true
+
+require 'formula'
 
 class SdLocal < Formula
-  desc "sd-local"
-  homepage "https://screwdriver.cd/"
-  version "1.0.40"
-  url "https://github.com/screwdriver-cd/sd-local/releases/download/v#{version}/sd-local_darwin_amd64"
-  sha256 "ea613eb2bc53871878cffc5f1798091c0ffaeedea876b9e8f67c6be432e784cf"
+  desc 'sd-local'
+  homepage 'https://screwdriver.cd/'
+  version '1.0.41'
+  url "https://github.com/screwdriver-cd/sd-local/releases/download/v#{version}/sd-local_darwin_all"
+  sha256 '3428647d045c93724657009b730a337d99d73b758f3628b29d278c77e43776da'
 
   def install
-    bin.install "sd-local_darwin_amd64" => "sd-local"
+    bin.install 'sd-local_darwin_all' => 'sd-local'
   end
 end
